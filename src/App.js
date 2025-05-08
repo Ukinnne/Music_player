caf0f9a50722178f117ccc7852f288bf1916d4c4
Ukinnne <osolovegor@gmail.com>
2025-05-08 20:01:58 +1000

    Song start/stop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 const playButton = document.getElementById('play-button');
-        const playIcon = document.getElementById('play-icon');
-        const pauseIcon = document.getElementById('pause-icon');
-        let isPlaying = false;
+const playIcon = document.getElementById('play-icon');
+const pauseIcon = document.getElementById('pause-icon');
+const audioPlayer = document.getElementById('audio-player'); // Получаем аудиоэлемент
+let isPlaying = false;
 
-        playButton.addEventListener('click', function() {
-            isPlaying = !isPlaying;
-            
-            if (isPlaying) {
-                playIcon.style.display = 'none';
-                pauseIcon.style.display = 'block';
-            } else {
-                playIcon.style.display = 'block';
-                pauseIcon.style.display = 'none';
-            }
-        });
+playButton.addEventListener('click', function() {
+    isPlaying = !isPlaying;
+    
+    if (isPlaying) {
+        playIcon.style.display = 'none';
+        pauseIcon.style.display = 'block';
+        audioPlayer.play(); // Запуск музыки
+    } else {
+        playIcon.style.display = 'block';
+        pauseIcon.style.display = 'none';
+        audioPlayer.pause(); // Остановка музыки
+    }
+});
